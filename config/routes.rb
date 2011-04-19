9705ef86673006d60b0314f86ecbca6bbe37c8f8
Paul Selden <pselden@myspace-inc.com>
2011-04-19 15:16:35 -0400

    Pulling header into a partial + minor route refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 CrystalGravy::Application.routes.draw do
 
   resources :users
-
   resources :playlists
+
   get "playlists/new"
 
   get "songs/new"
 
-  get "pages/home"
-  get "pages/test"
-
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
+
   root :to => "pages#home"
 
 end
