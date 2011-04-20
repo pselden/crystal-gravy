72ddfa37a4575d82481dd4eceb798b1b73a6ab97
Nouman Saleem <nsaleem@myspace-inc.com>
2011-04-20 13:50:41 -0700

    Show.hbs for playlist show method + some css changes + loading users playlists for profile (not surfaced yet)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    respond_with(:user => {:name => @user.name, :image => @user.image, :is_owner => owner?(params[:id]) })
+    @user = User.find(params[:id], :include => :playlists)
+    respond_with(:user => {:name => @user.name, :image => @user.image, :is_owner => owner?(params[:id])})
   end
 end
