6fec253dad6973b9c867a9618d2953090527ad75
Nouman Saleem <nsaleem@myspace-inc.com>
2011-04-20 07:23:37 -0700

    Sessions_helper: owner?(accepts user_id)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    respond_with(:user => {:name => @user.name, :image => @user.image, :is_owner => signed_in? && current_user.id.to_s() == params[:id] ? true : false })
+    respond_with(:user => {:name => @user.name, :image => @user.image, :is_owner => owner?(params[:id]) })
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,8 +2,19 @@ module SessionsHelper
 	def signed_in?
 		!current_user.nil?
 	end
-	
+
 	 def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+   end
+
+   def owner?(user_id)
+     if signed_in?
+       if user_id.is_a? Integer
+         user_id == current_user.id ? true : false
+       else
+         user_id == current_user.id.to_s ? true : false
+       end
+     end
+   end
+
 end
