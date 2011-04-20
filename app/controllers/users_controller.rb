dc6348fb3dc5a77ead7aa800be3488e1e0b6905c
Nouman Saleem <Nouman6@sbcglobal.net>
2011-04-20 16:43:16 -0700

    Users now showing playlists on profile. Need some json/handlebars love.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id], :include => :playlists)
-    respond_with(:user => {:name => @user.name, :image => @user.image, :is_owner => owner?(params[:id])})
+    respond_with(:user => {:name => @user.name, :image => @user.image, :playlists => @user.playlists, :is_owner => owner?(params[:id])})
   end
 end
