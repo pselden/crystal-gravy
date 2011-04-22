e6c9c58763056d1e6848f7716ff622760267db92
Nouman Saleem <Nouman6@sbcglobal.net>
2011-04-22 08:54:48 -0700

    Load playlists (anything really) with pretty 'seo' URL through helper method 'id_from_url'. 'playlists/Dinner_Mix-1' will work now.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,17 +2,17 @@ class PlaylistsController < ApplicationController
   def new
 		respond_with(:form => {:header => "New playlist", :token => form_authenticity_token() } )
   end
-  
+
   def index
     @playlist = Playlist.all
     respond_with(@playlist)
   end
 
   def show
-	  @playlist = Playlist.find(params[:id])
+	  @playlist = Playlist.find_by_id(id_from_url(params[:id]))
     respond_with(@playlist)
   end
-	
+
 	def create
 		@playlist = Playlist.new(params[:title])
 		@playlist.save
--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -2,4 +2,8 @@ module PlaylistsHelper
   def playlist_url(name, id)
     "/playlists/#{name.gsub(/ /,'_')}-#{id.to_s}"
   end
+
+  def id_from_url(params_url)
+    params_url.split("-")[-1]
+  end
 end
