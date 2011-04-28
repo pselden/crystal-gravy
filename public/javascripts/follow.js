50d4a0e8ce98b5e93b6021e1529c3670fd78a2ec
Nouman Saleem <nsaleem@myspace-inc.com>
2011-04-28 14:16:54 -0700

    primary buttons

--- public/javascripts/follow.js
+++ public/javascripts/follow.js
@@ -4,6 +4,7 @@
     var $this = $(this),
         user_id = $this.attr('data-following_id'),
         action = ($this.attr('data-icon') == "+") ? "follow" : "unfollow";
+    $this.attr('disabled', 'disabled');
     $.ajax({
       url: (action == "follow") ? "/followings/create" : "/followings/destroy",
       type: (action == "follow") ? "post" : "delete",
@@ -13,10 +14,12 @@
         if (action == "follow")
         {
           $this.attr('data-icon', '-')
-            .text('Unfollow');
+            .text('Unfollow')
+            .removeAttr('class');
         } else {
            $this.attr('data-icon', '+')
-            .text('Follow');
+            .text('Follow')
+            .attr('class', 'primary');
         }
       }
   });
