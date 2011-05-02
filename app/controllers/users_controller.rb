9f9f0640338e53ee05c468c557f16d0a46cf07d4
Nouman Saleem <nsaleem@myspace-inc.com>
2011-05-02 10:35:43 -0700

    Changing back until we fix the cleaner way of doing it. (issue was getting user objects for follower/following records instead of the actual relationship record)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,40 @@ class UsersController < ApplicationController
 
   def show
     append_javascript('follow')
-    @user = User.find_by_id(params[:id], :include => [:followers, :followings, :playlists])
+    @user = User.find(params[:id], :select => "id, name, image")
     @title = @user.name
-    result = @user.as_json(:include => [:followers, :followings, :playlists])
-    result["user"].merge!(:is_owner => owner?(params[:id]), :following => signed_in? ? current_user.following?(@user.id) : false)
-    respond_with(result)
-  end
+    #get playlists
+    @playlists = Array.new
+    @user.playlists.find(:all, :select => "playlists.id, playlists.titlename").each do |playlist|
+      @playlists << {
+        "id" => playlist.id,
+        "name" => playlist.titlename,
+        "url" => playlist_url(playlist.titlename, playlist.id)
+      }
+    end
+    #get followers
+    @followers = Array.new
+    @user.followers.find(:all, :select => "follower_id").each do |follower|
+      follower = User.find_by_id(follower.follower_id, :select => "id, name, image")
+      @followers << {
+        "id" => follower.id,
+        "name" => follower.name,
+        "image" => follower.image
+      }
+    end
+    #get following
+    @following = Array.new
+    @user.followings.find(:all, :select => "following_id").each do |following|
+      following = User.find_by_id(following.following_id, :select => "id, name, image")
+      @following << {
+        "id" => following.id,
+        "name" => following.name,
+        "image" => following.image
+      }
+    end
 
+    respond_with(:user => {:id => @user.id, :name => @user.name, :image => @user.image, :playlists => @playlists, :followers => @followers.empty? ? nil : @followers, :followings => @following.empty? ? nil : @following, :is_owner => owner?(params[:id]), :following => signed_in? ? current_user.following?(@user.id) : false})
+  end
 
   def edit
     if signed_in?
@@ -23,7 +50,6 @@ class UsersController < ApplicationController
       #redirect to signin?
     end
   end
-
 
   def create
     @user = User.create(params[:user])
